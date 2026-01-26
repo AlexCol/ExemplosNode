9ca0e9f0f6fd5c38f662b921ee3311070c25c225
Alexandre Coletti <alexandre.coletti@controlsoft.com.br>
2026-01-26 16:40:34 -0300

    .

--- N_NestJs/02_RabbitMQ/src/app.module.ts
+++ N_NestJs/02_RabbitMQ/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { ConsumersModule } from './rabbit-consumers/consumers.module';
 import { ServerModule } from './rabbit-server/server.module';
 
 @Module({
   imports: [ConsumersModule, ServerModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
