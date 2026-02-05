d48a3cfc83cb3bf081d9d616ad6209971edc43d0
AlexCol <eu_axil@yahoo.com.br>
2026-02-05 14:03:32 -0300

    adicionando client (backend) para servidor de traduções

---
+++ 04_Traducoes/03_Terceiro_Consumir TranslatorServer/src/index.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import { dicionario, formatTranslation, loadDicionario } from './translations';
+import { Langs } from './translations/types';
+
+async function bootstrap() {
+  await loadDicionario();
+
+  const app = express();
+
+  app.get('', (req, res) => {
+    const lang = (req.query.lang as Langs) || 'pt-BR';
+    res.json({
+      mensagem: dicionario[lang].usuarios['Bem-vindo, {{name}}'],
+      com_formato: formatTranslation(dicionario[lang].usuarios['Bem-vindo, {{name}}'], { name: 'João' }),
+      chave_fallback: dicionario[lang].usuarios['Chave Usuario'],
+    });
+  });
+
+  app.listen(3001, () => {
+    console.log('Server running on port 3001');
+  });
+}
+
+bootstrap();
